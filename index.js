c3b0253ebf70e336e8c8593a9c88b9cac69da597
Nilson Cardoso <nicarbessa@gmail.com>
2025-03-13 16:08:46 +0000

    setup inicial do FireBase

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+dotenv.config();
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6IuBVz7Uk4zGMH6iKZPioC9FFZuYwEhs",
-  authDomain: "travelex-aedc8.firebaseapp.com",
-  projectId: "travelex-aedc8",
-  storageBucket: "travelex-aedc8.firebasestorage.app",
-  messagingSenderId: "662096626912",
-  appId: "1:662096626912:web:b6f780afa6591a0b7e3c85",
-  measurementId: "G-J7CETLQHL2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
-//console.log(app);
